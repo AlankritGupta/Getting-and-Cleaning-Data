d6dad1f58402ad1e8934fb401e562b7acc417542
AlankritGupta <alankrit.777.ds@gmail.com>
2016-08-19 16:53:18 +0530

    Delete run_analysis.R

--- run_analysis.R
+++
@@ -1,9 +0,0 @@
-library(data.table)
-
-
-
-
-labels <- read.table("activity_labels.txt")
-labels[,2] <- as.character(labels[,2])
-features <- read.table("features.txt")
-features[,2] <- as.character(features[,2])
